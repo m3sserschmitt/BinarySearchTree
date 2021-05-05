eebc891b7625abfd4d1e9433c0befc28494e7b84
Romulus-Emanuel Ruja <romulus-emanuel.ruja@tutanota.com>
2021-05-05 10:50:24 +0300

    tests

--- main.cc
+++ main.cc
@@ -1,61 +1,87 @@
 #include <iostream>
-#include <list>
-#include <set>
+#include <chrono>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <fstream>
 
 #include "s.hh"
 
 using namespace std;
 
-int main()
+uint64_t milliseconds()
+{
+    uint64_t ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::
+                  now().time_since_epoch()).count();
+    return ms; 
+}
+
+uint64_t microsseconds()
+{
+    uint64_t us = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::
+                  now().time_since_epoch()).count();
+    return us; 
+}
+
+void insert_range(S &s, int m, int l, ofstream &outfile)
+{
+    clock_t start;
+    double avg = 0;
+    int p = 10;
+    double sup = 0;
+
+    for(int i = m; i < l; i ++)
+    {
+        start = clock();
+        s.insert(i);
+        avg += clock() - start;
+
+        if(not (i % p))
+        {
+            avg /= p;
+
+            if(avg > sup/* or not(i % ((m - l) / 100))*/)
+            {
+                sup = avg;
+                outfile << i << " " << sup << "\n";
+            }
+
+            avg = 0;
+        }
+    }
+}
+
+void insert_random(S &s, int m, int l, size_t count)
+{
+    srand( (unsigned)time(NULL) );
+    clock_t start;
+    int number;
+    for(int i = 0; i < count; i ++){
+        number = (rand() + m) % l;
+        start = clock();
+        s.insert(number);
+        cout << clock() - start << "\n";
+        count --;
+        
+    }
+    // cout << ((double)milliseconds() - (double)start) << "\n";
+}
+
+int main(int argc, char **argv)
 {   
-    list<int> elements = {1, 2, 3, 4, 5, 3, 10, -2, -4, 2, 1, 4, 3};
-    set<int> dist_elements;
-
-    list<int>::iterator it = elements.begin();
-    list<int>::iterator it_end = elements.end();
-
     S s;
+    string op = string(argv[1]);
 
-    for (; it != it_end; it++)
+    ofstream outfile = ofstream();
+    outfile.open(op + "_out");
+
+    if(op == "insert_range")
     {
-        s.insert(*it);
-        dist_elements.insert(*it);
-    }
-
-    cout << s.kth_element(7) << "\n";
-
-    // cout << s.exists(200) << "\n";
-
-    cout << "tree: " << s << "\n";
-
-   /* cout << "min: " << s.minimum() << '\n';
-    cout << "max: " << s.maximum() << '\n';
-
-    set<int>::iterator dist_it = dist_elements.begin();
-    set<int>::iterator dist_it_end = dist_elements.end();
-
-    for (; dist_it != dist_it_end; dist_it++)
+        insert_range(s, atoi(argv[2]), atoi(argv[3]), outfile);
+    } else if (op == "insert_random")
     {
-        try
-        {
-            cout << "predecessor of " << *dist_it << " is: " << s.predecessor(*dist_it) << "\n";
-        }
-        catch (const char *err)
-        {
-            cout << err << "\n";
-        }
-
-        try
-        {
-            cout << "successor of " << *dist_it << " is: " << s.successor(*dist_it) << "\n";
-        }
-        catch (const char *err)
-        {
-            cout << err << "\n";
-        }
-
-        cout << "\n";
+        insert_random(s, atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
     }
-*/
+    
     return 0;
 }
